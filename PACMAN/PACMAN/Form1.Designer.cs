54bfb16c5958e64ac396ac0702a8eb2d5dd2cab7
ncoro <ncoro@DESKTOP-DB08PRK>
2022-06-01 23:21:36 +0200

    adding intro with the button

--- PACMAN/PACMAN/Form1.Designer.cs
+++ PACMAN/PACMAN/Form1.Designer.cs
@@ -29,13 +29,43 @@ namespace PACMAN
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BPlay
+            // 
+            this.BPlay.BackColor = System.Drawing.Color.Black;
+            this.BPlay.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.BPlay.FlatAppearance.BorderSize = 5;
+            this.BPlay.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BPlay.ForeColor = System.Drawing.Color.Yellow;
+            this.BPlay.Location = new System.Drawing.Point(333, 53);
+            this.BPlay.Name = "BPlay";
+            this.BPlay.Size = new System.Drawing.Size(190, 60);
+            this.BPlay.TabIndex = 0;
+            this.BPlay.Tag = "playButton";
+            this.BPlay.Text = "PLAY";
+            this.BPlay.UseVisualStyleBackColor = false;
+            this.BPlay.Click += new System.EventHandler(this.BPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::PACMAN.Properties.Resources.intro;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.BPlay);
+            this.Name = "Form1";
+            this.Text = "Pac Man";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BPlay;
     }
 }
 
--- PACMAN/PACMAN/Form1.cs
+++ PACMAN/PACMAN/Form1.cs
@@ -12,10 +12,17 @@ namespace PACMAN
 {
     public partial class Form1 : Form
     {
+        enum STATE { START, GAME, LOST, WIN};
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void BPlay_Click(object sender, EventArgs e)
+        {
+             BPlay.Visible = false;
+             BackgroundImage = null;
+             BackColor = Color.Black;
+        }
     }
 }
